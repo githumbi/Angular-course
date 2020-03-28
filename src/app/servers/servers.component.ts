5ca4c801ba0a6546741656c048bc97cf43d663fa
githumbi <githumbi74@gmail.com>
2020-03-28 20:23:16 +0300

    done

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,24 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
-  serverInfo = 'button not clicked';
-  serverName = 'sample text';
-
-  constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
-  }
+username = 'joe';
+  constructor() { }
 
   ngOnInit(): void {
   }
-  // event binding
-  onCreateStatus() {
-    this.serverInfo = 'button clicked!' + this.serverName;
-  }
-// passing and using data with data binding
-  onUpdateServerName(event: Event) {
-    this.serverName = (<HTMLInputElement>event.target).value;
-  }
+
 }
